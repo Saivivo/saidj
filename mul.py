8323a20b5f8c172b1c3be97a1fc80c592b161e58
Saivivo <hemanth99515@gmail.com>
2021-11-24 21:28:28 +0530

    Fourt commit by sai

--- mul.py
+++ mul.py
@@ -3,6 +3,7 @@ n = int(input('Enter a with multiplycation table:'))
 a = int(input('Enter a any number:'))
 print('*'*50)
 print('Multiplycation Table',n)
+print('*'*50)
 for i in range(1,a+1):
 	print('\t{0} * {1} = {2}'.format(n,i,n*i))
 print('*'*50)
